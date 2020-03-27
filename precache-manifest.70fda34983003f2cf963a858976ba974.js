1a8b543747ccb3cf4a2650cfb4389c415152d1fb
Alexandre Salsinha <alexsalsinha@gmail.com>
2020-03-27 12:25:22 +0000

    Updates

---
+++ precache-manifest.70fda34983003f2cf963a858976ba974.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caee83e2ee5e571eddca4fc88808b424",
+    "url": "/wwnews/index.html"
+  },
+  {
+    "revision": "637fc4b4ec0c2f89663f",
+    "url": "/wwnews/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "e9c218be56f28e14df55",
+    "url": "/wwnews/static/css/main.9b5399b3.chunk.css"
+  },
+  {
+    "revision": "637fc4b4ec0c2f89663f",
+    "url": "/wwnews/static/js/2.a0c3caa5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/wwnews/static/js/2.a0c3caa5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9c218be56f28e14df55",
+    "url": "/wwnews/static/js/main.6e5988a7.chunk.js"
+  },
+  {
+    "revision": "9d3da73714c42de08bd0",
+    "url": "/wwnews/static/js/runtime-main.a264e32e.js"
+  }
+]);
